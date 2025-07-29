51c017b75102203783c23ed346d47dd15b8ce566
jinho3085 <jinho3085@naver.com>
2025-07-29 15:56:14 +0900

    java.util #2

---
+++ chapter04/src/main/java/chapter04/util/DiceSimulator.java
@@ -0,0 +1,20 @@
+package chapter04.util;
+
+import java.util.Random;
+
+public class DiceSimulator {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		
+//		String key = "";
+		for(int i = 0; i < 20; i++) {
+			int dice = rand.nextInt(6) + 1; // 1 ~ 6
+//			key += dice;
+			System.out.println("주사위 결과 :" +  dice);
+		}		
+		
+//		System.out.println(key);
+	}
+
+}
